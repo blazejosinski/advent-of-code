59c78dbdd496a8d124e757b6839a58bfd4e6f3ab
Błażej Osiński <blazej.osinski@gmail.com>
2021-12-01 13:11:03 +0100

    First day solution.

---
+++ day1.py
@@ -0,0 +1,22 @@
+import re
+
+from helpers import solve_line_task 
+
+def seat_number(s):
+  return int(s)
+
+def count_increase(l):
+  pairs = zip(l, l[1:])
+  deeper = [x < y for x,y in pairs]
+  return sum(deeper)
+
+def count_increase_groups(l):
+  sums = [a+b+c for a,b,c in zip(l, l[1:], l[2:])]
+  return count_increase(sums)
+
+def main():
+  print(solve_line_task("in0", seat_number, count_increase_groups))
+  print(solve_line_task("in1", seat_number, count_increase_groups))
+
+if __name__ == "__main__":
+    main()
